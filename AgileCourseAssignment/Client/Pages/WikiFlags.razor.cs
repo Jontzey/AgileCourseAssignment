2e559412f7e959d3b1dae75baa2ffb8a1206660c
Mihaela Maxim <maxim.mihaela1996@gmail.com>
2023-09-13 11:17:44 +0200

    Update

--- AgileCourseAssignment/Client/Pages/WikiFlags.razor.cs
+++ AgileCourseAssignment/Client/Pages/WikiFlags.razor.cs
@@ -12,31 +12,12 @@ namespace AgileCourseAssignment.Client.Pages
         //private IFlagService wikiService { get; set; }
         private static List<FlagsModel> flagList = new();
 
-        private List<bool> isFlippedList = new List<bool>();
-
-
         protected override async void OnInitialized()
         {
             flagList = await wikiService.GetAllFlags();
-            isFlippedList = Enumerable.Repeat(false, flagList.Count).ToList();
 
         }
-
-
-        private void ToggleCard(int index)
-        {
-            if (index >= 0 && index < isFlippedList.Count)
-            {
-                isFlippedList[index] = !isFlippedList[index];
-                StateHasChanged(); // Trigger a re-render to update the UI
-            }
-            else
-            {
-                // Handle the index out of range error gracefully.
-                // You can display an error message or take appropriate action.
-            }
-        }
-
+      
         private void NavigateToHome()
         {
             Navigation.NavigateTo("/");
